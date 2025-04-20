7e5141c534c388734639895da9eda59c2b2af809
MehdiHachicha <mhachicha@outlook.com>
2025-04-20 11:21:34 -0400

    feat(product-listing-page): display the product cards

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,8 +274,22 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
-
+                    {plantsArray.map((category, index) => (
+                        <div key={index}>
+                            <h1><div className='category'>{category.category}</div></h1>
+                            <div className="product-list">
+                                {category.plants.map((plant, plantIndex) => (
+                                <div className="product-card" key={plantIndex}>
+                                    <img className="product-image" src={plant.image} alt={plant.name} />
+                                    <div className="product-title">{plant.name}</div>
+                                    <div className="product-price">{plant.cost}</div>
+                                    <p>{plant.description}</p>
+                                    <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
